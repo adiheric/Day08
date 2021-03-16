84ee5451d6f514d477b206120a1ec5b1efe8183c
adiheric <hadiheric@gmail.com>
2021-03-16 10:41:54 +0100

    day08

---
+++ ex12/src/index.js
@@ -0,0 +1,7 @@
+var randomNumberBetween0and19 = Math.floor(Math.random() * 20);
+function randomInteger(){
+return randomInteger(Math.random() * 30);
+
+}
+console.log(randomInteger());
+module.exports = randominteger;
